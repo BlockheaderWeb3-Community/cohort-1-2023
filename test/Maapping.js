be3b6243b304478344080f760ac36466493b0e2c
Isaac Wanger <teryima127@gmail.com>
2023-04-07 06:52:08 +0100

    test cases

--- test/Maapping.js
+++ test/Maapping.js
@@ -11,12 +11,29 @@ describe("Mapping Test Suite", async () => {
         [owner, addr1, addr2, mappingContract] = await ethers.getSigners();
         const MappingContract = await ethers.getContractFactory("Mapping")
         mappingContract = await MappingContract.deploy(owner.address)
-    })
+    });
 
-    // Test case for deployment
+    
+  // Before each test, deploy a new Mappings contract and get the owner
     describe("Deployment", async () => {
         it("should set the right owner", async function () {
             expect(await mappingContract.owner()).to.equal(owner.address);
         })
     })
+
+    // Test adding and retrirving 
+    it("should add and retrieve retrieve multiple students", async function () {
+        // Add a new student
+        const id = 1;
+        const name = "Isaac";
+        await mappingContract.addStudent(id, name);
+
+        // Retrieve the student's name
+        const studentName = await mappingContract.viewSutudent(id);
+
+        // Check that name is correct
+        expect(studentName).to.equal(name);
+    });
+
+    // Test add
 })
